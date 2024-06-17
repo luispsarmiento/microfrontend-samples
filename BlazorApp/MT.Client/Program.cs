fc5b0b6dffa23bebad245ab6cc1326be195d2376
Luis Puc <j.louis1493@gmail.com>
2024-06-17 04:01:10 +0000

    Added: Module Federation to blazor app

--- BlazorApp/MT.Client/Program.cs
+++ BlazorApp/MT.Client/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MT.Client;
+using Blazor.ModuleFederation.Angular;
+using MT.Client.Pages;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.RootComponents.RegisterForModuleFederation<Counter>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Experiments on the microfrontend architecture
 - In the server project, add references to the client app and the shared library.
 - In the client project, add a reference to the shared library.
 - Comment out the `launchURL` attribute in the launch profiles.
+- To configure the blazor app to export component for a microfrontend architecture install `Blazor.ModuleFederation.Angular` at Client project.
